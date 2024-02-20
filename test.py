b2bd5cd1251f52452dadb36f18542580fafaf925
Tianjian Shen <2300011417@stu.pku.edu.cn>
2024-02-20 16:40:39 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,4 @@
+git config --global user.name "Tianjian Shen"
+git config --global user.email "2300011417@stu.pku.edu.cn"
+https://github.com/FantasticSamuel/Tianjian-Shen-.git
+git remote add origin https://github.com/FantasticSamuel/T
