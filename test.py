55e3c56be27662dd8e49832d587aa68511dc24bb
Tianjian Shen <2300011417@stu.pku.edu.cn>
2024-02-20 17:00:52 +0800

    Your commit message

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 git config --global user.name "Tianjian Shen"
 git config --global user.email "2300011417@stu.pku.edu.cn"
 https://github.com/FantasticSamuel/Tianjian-Shen-.git
-git remote add origin https://github.com/FantasticSamuel/T
